dd7bed7e0535dbfd1fecf64758283ddd0ee64a60
Just-Jannik-03 <jannik.eurich@htwg-konstanz.de>
2026-06-23 18:23:25 +0200

    GitHub Pages deployment setup

--- easydocvue/src/main.ts
+++ easydocvue/src/main.ts
@@ -25,7 +25,7 @@ const auth0 = createAuth0({
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   authorizationParams: {
     audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-    redirect_uri: window.location.origin + import.meta.env.BASE_URL,
+    redirect_uri: window.location.origin + window.location.pathname,
   },
 })
 
